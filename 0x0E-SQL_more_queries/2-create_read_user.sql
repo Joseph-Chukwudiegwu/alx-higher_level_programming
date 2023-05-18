2cf8a5d83598aa813c8c265f3484fff89a2bcb50
joseph-chukwudiegwu <brainseph04@gmail.com>
2023-05-18 01:26:54 -0700

    correction

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
---script to run msql commands
+-- script to run msql commands
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
